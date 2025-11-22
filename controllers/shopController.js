c0c771a8af808d8384fdcc52c3b96cb9edcafd13
triptishakya-dev <triptishakya.2001@gmail.com>
2025-11-23 02:19:28 +0530

    post api done

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -1,1 +1,52 @@
-export const 
+export const addShop = async (req, res) => {
+  try {
+    const {
+      Name,
+      ownerName,
+      Address,
+      Phone,
+      Email,
+      City,
+      State,
+      pincode,
+      gstNumber,
+      category,
+    } = req.body;
+
+    if (
+      !Name ||
+      !ownerName ||
+      !Address ||
+      !Phone ||
+      !Email ||
+      !City ||
+      !State ||
+      !pincode ||
+      !gstNumber ||
+      !category
+    ) {
+      return res.Status(400).json({ message: " all fields required" });
+    }
+
+    const newShop = await prisma.Shop.create({
+      data: {
+        Name,
+        ownerName,
+        Address,
+        Phone,
+        Email,
+        City,
+        State,
+        pincode,
+        gstNumber,
+        category,
+      },
+    });
+    res.status(201).json({
+      message: "shop created successfully",
+      addShop: newShop,
+    });
+  } catch (error) {
+    console.log("error");
+  }
+};
--- routers/shopRoute.js
+++ routers/shopRoute.js
@@ -1,6 +1,8 @@
 import express from "express";
+import { addShop } from "../controllers/shopController";
 
 
 const router = express.Router();
+router.post("/", addShop);
 
 export default router;
